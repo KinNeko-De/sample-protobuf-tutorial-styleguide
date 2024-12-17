bcdf484d8a967846b1a86c96330388ff4296f3d2
Nils Diekmann <github@kinneko.de>
2024-12-18 00:08:30 +0100

    Generate code for csharp

---
+++ csharp/sample/KinNekoDe.ApiContract.Sample/FileName.cs
@@ -0,0 +1,24 @@
+﻿using S000.V1;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace KinNekoDe.ApiContract.Sample;
+internal class FileName
+{
+    public void Test()
+    {
+        var test = new S611.V1.OneOnf()
+        {
+        };
+        test.Natural = new S611.V1.NaturalPerson()
+        {
+            FamilyName = "test",
+            GivenName = "test",
+        };
+        test.Company = new S611.V1.LegalPerson()
+        {
+            Name = "test",
+        };
+    }
+}
